aed38898a291de40e0ca8a307ac11fd5b3036b72
Wu-Jun Pei <b06902029@csie.ntu.edu.tw>
2019-02-21 16:00:14 +0000

    2nd Attempt on Feb 21
    Linear Regression w/ Gradient

--- HW1/LinearRegression.py
+++ HW1/LinearRegression.py
@@ -6,7 +6,7 @@ import pandas as pd
 def ReadTrainingData(path):
     df = pd.read_csv(path, encoding="big5").drop(["測站"], axis = 1)
     df = df.loc[df["測項"] == "PM2.5"]
-    data = df.drop(columns = ["日期", "測項"]).values.astype(float).reshape(-1)
+    data = df.drop(columns = ["日期", "測項"]).values.astype(np.float64).reshape(-1)
 
     num = data.shape[0] - 9
     X = np.concatenate([data[i : i + 9]       for i in range(num)], axis = 0).reshape((num, 9))
@@ -16,6 +16,9 @@ def ReadTrainingData(path):
 
     return X, Y
 
+def Loss(w, X, Y):
+    return (np.sum((np.dot(X, w.T) - Y) * (np.dot(X, w.T) - Y)) / X.shape[0]) ** 0.5
+
 def Grad(w, X, Y):
     return np.sum(2 * (Y - np.dot(X, w.T)) * (-X), axis = 0)
 
@@ -23,15 +26,16 @@ X, Y = ReadTrainingData("data/train.csv")
 
 num, dim = X.shape
 
-eta = 1e-7
-iterationTimes = int(1e5) 
+eta = 2e-8
+iterationTimes = 5 * (10 ** 5)
 
 w = np.zeros((1, dim))
 
 for epoch in range(iterationTimes):
-    if epoch % 1000 == 0:
-        print("--- Epoch: %5d ---" % epoch)
     w -= eta * Grad(w, X, Y)
+
+    if epoch % 1000 == 0:
+        print("- Epoch: %6d, loss = %f" % (epoch, Loss(w, X, Y)))
 
 print(w)
 np.save("123.npy", w)
--- HW1/README.md
+++ HW1/README.md
@@ -11,5 +11,17 @@ Linear Regression with Gradient Descent
 - Parameters
     - Learning Rate $\eta$: `1e-8`
     - Iteration Time: `1e5`
-- Features: PM2.5 values of previous 9 hours
+- Features: PM2.5 values of previous 9 hours (without bias $x_0$)
 - Optimizer: None
+- Score = `5.99011`
+
+#### 2nd Attempt
+
+Linear Regression with Gradient Descent
+
+- Parameters
+    - Learning Rate $\eta$: `1e-8`
+    - Iteration Time: `1e6`
+- Features: PM2.5 values of previous 9 hours (with bias $x_0$)
+- Optimizer: None
+- Score = `5.93022`
