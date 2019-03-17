1fdee147d00cae2e08bd0d7dadbf51756c8281af
WJPei <b06902029@csie.ntu.edu.tw>
2019-03-17 22:00:04 +0800

    Add Gradient Boosting with SKLearn

---
+++ hw2/Work/LR_SK/train.py
@@ -0,0 +1,49 @@
+import sys
+import numpy as np
+import pandas as pd
+import pickle as pk
+from sklearn.ensemble import GradientBoostingClassifier
+
+def ReadTrainingData(path_X, path_Y, Q = 2):
+    squaredTerms = ["age", "capital_gain", "capital_loss", "hours_per_week"]
+    dfX = pd.read_csv(path_X).drop(["fnlwgt"], axis = 1)
+    for q in range(2, Q + 1):
+        for t in squaredTerms:
+            dfX[t + "^%d" % (q)] = dfX[t] ** q
+
+    dfY = pd.read_csv(path_Y)
+
+    rX = dfX.values.astype(np.float64)
+    rY = dfY.values.astype(np.float64).reshape((-1, 1))
+
+    data = np.concatenate([rX, rY], axis = 1)
+    np.random.shuffle(data)
+    rX, rY = data[:, :-1], data[:, -1].reshape((-1, 1))
+
+    mean = np.mean(rX, axis = 0).reshape((1, -1))
+    stdd = np.std(rX, axis = 0).reshape((1, -1))
+    zX = (rX - mean) / stdd
+    return np.concatenate([np.ones((zX.shape[0], 1)), zX], axis = 1), rY, mean, stdd
+
+
+if __name__ == "__main__":
+    Xtrain_csv = "../../data/X_train.csv"
+    Ytrain_csv = "../../data/Y_train.csv"
+    np_file = "result.npz"
+
+    X, Y, mean, stdd = ReadTrainingData(Xtrain_csv, Ytrain_csv, Q = 1)
+
+    num, dim = X.shape[0] * 3 // 4, X.shape[1]
+    
+    trainX, trainY = X[:num, :], Y[:num, :].reshape(-1)
+    validX, validY = X[num:, :], Y[num:, :].reshape(-1)
+
+    eta = 1e-1
+    clf = GradientBoostingClassifier(n_estimators = 100, learning_rate = eta, max_depth = 5, random_state = 31415926).fit(trainX, trainY)
+
+    print(clf.score(trainX, trainY))
+    print(clf.score(validX, validY))
+
+    np.savez(np_file, mean = mean, stdd = stdd)
+    with open("model.pkl", "wb") as f:
+        pk.dump(clf, f) 
