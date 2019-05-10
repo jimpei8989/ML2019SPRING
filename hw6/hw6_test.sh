2a4bd8d63dafdc35e010f88a42adbda219df5a2a
Wu-Jun <wjpei@csie.ntu.edu.tw>
2019-05-10 16:17:25 +0800

    Update test script

--- hw6/hw6_test.sh
+++ hw6/hw6_test.sh
@@ -5,28 +5,8 @@ predictY=$3
 w2vModel="Reproduce/word2vec.model"
 modelH5="Reproduce/model.h5"
 
-# Download Model on Google Drive
-# Reference: https://www.matthuisman.nz/2019/01/download-google-drive-files-wget-curl.html
-## W2V model
-w2vID="1ewCBdiGRV__24GL1SDmJKvm4Y9WEe7N-"
-
-wget --save-cookies cookies.txt "https://docs.google.com/uc?export=download&id="${w2vID} -O- \
-             | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-
-wget --load-cookies cookies.txt -O ${w2vH5} \
-             'https://docs.google.com/uc?export=download&id='${w2vID}'&confirm='$(<confirm.txt)
-
-## RNN model
-modelID="1rSsWay_EVChFjttf5F3jcKnQn0po-L0g"
-
-wget --save-cookies cookies.txt "https://docs.google.com/uc?export=download&id="${modelID} -O- \
-             | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-
-wget --load-cookies cookies.txt -O ${modelH5} \
-             'https://docs.google.com/uc?export=download&id='${modelID}'&confirm='$(<confirm.txt)
-
-rm confirm.txt cookies.txt
-
+wget https://www.csie.ntu.edu.tw/~wjpei/HYML/hw6/word2vec.model -O ${w2vModel}
+wget https://www.csie.ntu.edu.tw/~wjpei/HYML/hw6/model.h5 -O ${modelH5}
 
 python3 Reproduce/test.py ${dictTxt} \
                           ${testX} \
